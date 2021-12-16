ab8fb3c31a64e1df88eea875d18070bc64941aee
Rose Durandisse <rosedurandisse@pursuit.org>
2021-12-16 16:11:23 -0500

    Updated App File

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import react from "react";
 import "./App.css";
 import { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Nav from "./Components/Nav"
 import Home from "./Components/Home"
-
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/people" component={People} />
+          <Route path='/locations' component={Locations} />
         </Switch>
       </div>
     )
