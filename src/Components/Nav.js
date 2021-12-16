59f615731c2e5437f776c4eb903052b748109acf
Rose Durandisse <rosedurandisse@pursuit.org>
2021-12-16 11:04:19 -0500

    Moved Image Location

---
+++ src/Components/Nav.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import BlackPantherImage from './BlackPantherImage.jpeg'
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav>
+                <div>
+                    <div>
+                        <Link to='/'>
+                            <img src=''></img>
+                        </Link>
+                    </div>
+                    <ul>
+                        <li>
+                            <Link to='/movies'>Movies</Link>
+                        </li>
+                        <li>
+                            <Link to='/people'>People</Link>
+                        </li>
+                        <li>
+                            <Link to='locations'>Locations</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
