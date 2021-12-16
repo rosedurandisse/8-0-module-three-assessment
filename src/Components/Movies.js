494dd0e6f27721810502bddbeebacdab6f7b9c61
Rose Durandisse <rosedurandisse@pursuit.org>
2021-12-16 13:18:26 -0500

    Movies List"

---
+++ src/Components/Movies.js
@@ -0,0 +1,71 @@
+import { Component } from "react";
+
+class Movies extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movieData: [],
+            userChoseAMovie: false,
+            selectedMovie: '',
+        };
+    }
+
+
+
+    componentDidMount() {
+        fetch('https://ghibliapi.herokuapp.com/films')
+            .then((response) => response.json())
+            .then((listOfMovies => this.setState({ movieData: listOfMovies })))
+    }
+
+    handleUserSelection = (event) => {
+        event.preventDefault();
+
+        if (event.target.value === 'no-movie') {
+            this.setState({
+                userChoseAMovie: false,
+            })
+        } else {
+
+            const chosenMovie = this.state.movieData.find((eachMovie) => (eachMovie.title === event.target.value))
+            this.setState({
+                userChoseAMovie: true,
+                selectedMovie: chosenMovie
+            })
+        }
+    }
+
+
+    render() {
+        const movieDropdown = this.state.movieData.map((eachMovie) => {
+            return (
+                <option key={eachMovie.id} value={eachMovie.title}>{eachMovie.title}</option>
+            )
+        })
+        const chosenMovieDetails =
+            <div>
+                <p>{this.state.selectedMovie.title}</p>
+                <p>Released Date:{this.state.selectedMovie.release_date}</p>
+                <p>{this.state.selectedMovie.description}</p>
+            </div>
+
+
+        return <div className='movies'>
+            <p>Select a Movie</p>
+            <label htmlFor='MovieList'>Movie</label>
+            <select
+                name="movieList"
+                id="MovieList"
+                onChange={this.handleUserSelection}
+            >
+                <option value='no-movie'> </option>
+                {movieDropdown}
+            </select>
+            {this.state.userChoseAMovie && <div>{chosenMovieDetails} </div>}
+
+
+        </div >
+    }
+}
+
+export default Movies;
